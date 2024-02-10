99d730aa72956b726d33d6aca26fc8aa638170ab
Alba070503 <97574964+Alba070503@users.noreply.github.com>
2024-02-10 19:47:11 -0400

    Update propietario(a) -actualizacion.js

--- plugins/propietario(a) -actualizacion.js
+++ plugins/propietario(a) -actualizacion.js
@@ -1,25 +1,35 @@
+// adartado por Diego (Aka: un ladrón de código)
+
 import axios from 'axios';
 let previousCommitSHA = '';
 let previousUpdatedAt = '';
-const owner = 'GataNina-Li';
-const repo = 'GataBot-MD';
-let handler = async (m, { conn, text, usedPrefix, command }) => {
+let previousCommitUser = ''; 
+const owner = 'BrunoSobrino';
+const repo = 'TheMystic-Bot-MD';
+const handler = async (m, {conn, text, usedPrefix, command}) => {
+ conn.sendMessage(m.chat, {text: `*[❗] Comando activado con éxito, te notificaremos cuando haya algun cambio en el repositorio oficial.*`}, {quoted: m});  
+try {
   async function checkRepoUpdates() {
     try {
-      const response = await axios.get(`https://api.github.com/repos/${owner}/${repo}/commits?per_page=1`)
-      const { sha, commit: { message }, html_url } = response.data[0]
+      const response = await axios.get(`https://api.github.com/repos/${owner}/${repo}/commits?per_page=1`);
+      const {sha, commit: {message}, html_url, author: { login } } = response.data[0];
 
       if (sha !== previousCommitSHA || message !== previousUpdatedAt) {
-        previousCommitSHA = sha
-        previousUpdatedAt = message
-    conn.sendMessage(m.chat, { text: `${lenguajeGB['smsAvisoIIG']()} 𝙀𝙇 𝙍𝙀𝙋𝙊𝙎𝙄𝙏𝙊𝙍𝙄𝙊 𝙃𝘼 𝙎𝙄𝘿𝙊 𝘼𝘾𝙏𝙐𝘼𝙇𝙄𝙕𝘼𝘿𝙊!!\n𝙍𝙀𝙋𝙊𝙎𝙄𝙏𝙊𝙍𝙄𝙊: ${html_url}\n*• 𝙈𝙀𝙉𝙎𝘼𝙅𝙀 𝘿𝙀 𝘾𝙊𝙈𝙈𝙄𝙏:* ${message}` }, { quoted: m })
-   } 
-  } catch (error) {
-    await m.reply(`${fg} 𝙀𝙍𝙍𝙊𝙍 𝘼𝙇 𝙑𝙀𝙍𝙄𝙁𝙄𝘾𝘼𝙍 𝙀𝙇 𝙍𝙀𝙋𝙊𝙎𝙄𝙏𝙊𝙍𝙄𝙊:'`, error.message)
-   }
+        previousCommitSHA = sha;
+        previousUpdatedAt = message;
+        previousCommitUser = login
+        conn.sendMessage(m.chat, {text: `*[❗] ¡El repositorio ha sido actualizado!*\n*- Repositorio:* ${html_url}\n*- Mensaje de commit:* ${message}\n*- Commit por:* ${login}`}, {quoted: m});
+      }
+    } catch (error) {
+      console.error(error)
+      m.reply('*[❗] Error al verificar el repositorio*');
+    }
   }
- setInterval(checkRepoUpdates, 60000)
+  setInterval(checkRepoUpdates, 6000);
+} catch (e) {
+m.reply(e)
 }
-handler.command = /^(actualizar|actualizacion)/i
-handler.rowner = true
-export default handler
+};
+handler.command = /^(actualizacion|actualizaciones)/i;
+handler.rowner = true;
+export default handler;
